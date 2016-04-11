e7a146a00c8e05ec6b3efa5f9274656edf61c8a2
Andrew Savka <a_sawka@yahoo.com>
2016-04-11 10:44:14 +0300

    Update collection name

--- routes/api/sync.js
+++ routes/api/sync.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var translationsService = require('../../my_modules/services/translations_service.js');
+var fs = require('fs');
 
 
 router.post("/", function(req, res, next) {
+  //fs.writeFile('../translations.json', JSON.stringify(req.body.translations));
   translationsService.import(req.body.translations)
     .then(function(result) {
       res.json({message: "ok"});
